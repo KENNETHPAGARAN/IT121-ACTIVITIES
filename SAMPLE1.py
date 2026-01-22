93835142fefd7cd31c14e6da372f98b46980e301
IT1R6-KENNETH JIM ELRYS G. PAGARAN <pagaran.kennethjimelrys10@gmail.com>
2026-01-22 15:03:05 +0800

    Create SAMPLE1.py

---
+++ SAMPLE1.py
@@ -0,0 +1,11 @@
+student_id = "2025300218"
+student_name = "KENNETH JIM ELRYS G.PAGARAN"
+course = "BS Information technology"
+year_level = "1st year"
+
+print("STUDENT INFORMATION")
+print("-------------------")
+print("Student ID:", student_id)
+print("Name      :", student_name)
+print("course    :", course)
+print("Year level:",year_level)
