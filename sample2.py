c9c6bdf844f17510469e1a31c6c212afe2222cc0
IT1R6-KENNETH JIM ELRYS G. PAGARAN <pagaran.kennethjimelrys10@gmail.com>
2026-01-22 15:10:46 +0800

    Create sample2.py

---
+++ sample2.py
@@ -0,0 +1,14 @@
+num1 = 15
+num2 = 5
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+divition = num1 / num2
+
+print("BASIC ARITHMETIC OPERATIONS")
+print("---------------------------")
+print("addition   :", addition)
+print("subtraction :", subtraction)
+print("multiplication:", multiplication)
+print("divition:", divition)
